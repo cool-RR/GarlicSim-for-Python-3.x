96d72dd83e8c10e359467bcc0c8929e8847bba6a
Ram Rachum <cool-rr@cool-rr.com>
2009-10-14 02:55:45 +0200

    -

--- src/garlicsim/general_misc/junk/test5.py
+++ src/garlicsim/general_misc/junk/test5.py
@@ -2,33 +2,33 @@ import time
 import garlicsim
 from simulation_packages import life
 
-
-state = life.make_random_state(10, 10)
-
-print(state)
-
-'''
-new_state = garlicsim.simulate(life, state, 10)
-
-print(new_state)
-
-result = garlicsim.list_simulate(life, state, 10)
-
-assert result[-1] == new_state
-'''
-
-
-project = garlicsim.Project(life)
-
-root = project.root_this_state(state)
-
-project.crunch_all_leaves(root, 20)
-
-print(project.sync_crunchers())
-
-for i in range(5):
-    time.sleep(1)
+if __name__ == '__main__':
+    state = life.make_random_state(10, 10)
+    
+    print(state)
+    
+    '''
+    new_state = garlicsim.simulate(life, state, 10)
+    
+    print(new_state)
+    
+    result = garlicsim.list_simulate(life, state, 10)
+    
+    assert result[-1] == new_state
+    '''
+    
+    
+    project = garlicsim.Project(life)
+    
+    root = project.root_this_state(state)
+    
+    project.crunch_all_leaves(root, 100)
+    
     print(project.sync_crunchers())
-
-
+    
+    for i in range(5):
+        time.sleep(1)
+        print(project.sync_crunchers())
+    
+    
     
