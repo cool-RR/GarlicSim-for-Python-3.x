bc787fa674b3f166d71186b647525aa693423158
Ram Rachum <cool-rr@cool-rr.com>
2011-01-11 21:55:55 +0200

    -

--- garlicsim_py3/test_garlicsim/test_general_misc/test_sleek_refs/test_cute_sleek_value_dict/test_generic_dict_tests.py
+++ garlicsim_py3/test_garlicsim/test_general_misc/test_sleek_refs/test_cute_sleek_value_dict/test_generic_dict_tests.py
@@ -564,7 +564,7 @@ class GenericDictTest(unittest2.TestCase):
 
         
     def test_bad_key(self):
-        if garlicsim.__version_info__ <= (0, 6, 0):
+        if garlicsim.__version_info__ <= (0, 6, 1):
             raise nose.SkipTest('Not looked at this yet.')
         # Dictionary lookups should fail if __cmp__() raises an exception.
         class CustomException(Exception):
